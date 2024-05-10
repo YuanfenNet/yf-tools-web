e8d91c6a97160d66b29c4bbb94b46b509aaaa225
Bean <dengbin@bilibili.com>
2024-05-10 18:11:48 +0800

    feat: add ffmpeg

---
+++ pages/metadata.vue
@@ -0,0 +1,8 @@
+<template>
+    <page header="音视频元数据解析" class="page-metadata"/>
+</template>
+
+<script setup lang="ts">
+import { FFmpeg } from '@ffmpeg/ffmpeg'
+import { toBlobURL } from '@ffmpeg/util'
+</script>
